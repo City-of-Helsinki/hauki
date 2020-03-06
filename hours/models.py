3f14891eb512081a6767ae1af721e4f1ebb3a0e6
Riku Oja <riku.oja@kapsi.fi>
2020-03-06 18:04:47 +0200

    Add indexes and migrations

--- hours/models.py
+++ hours/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import DateTimeRangeField
+from django.contrib.postgres.indexes import GistIndex
 from django.utils.translation import ugettext_lazy as _
 from hauki import settings
 
@@ -35,6 +37,8 @@ class DataSource(models.Model):
 
 class BaseModel(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
+
+    # Both fields are required
     data_source = models.ForeignKey(
         DataSource, on_delete=models.PROTECT, related_name='provided_%(class)s_data', db_index=True)
     origin_id = models.CharField(verbose_name=_('Origin ID'), max_length=100, db_index=True)
@@ -52,9 +56,14 @@ class BaseModel(models.Model):
     last_modified_by = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, blank=True,
         related_name="%(app_label)s_%(class)s_modified_by")
+    deleted = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
+        constraints = [
+            models.UniqueConstraint(fields=['data_source', 'origin_id'],
+                                    name='%(app_label)s_%(class)s_origin_id_unique'),
+        ]
 
     def __str__(self):
         return f'{self.name} ({self.id})'
@@ -63,10 +72,10 @@ class BaseModel(models.Model):
 class Target(BaseModel):
     parent = models.ForeignKey('self', on_delete=models.PROTECT, related_name='first_children', db_index=True, null=True)
     second_parent = models.ForeignKey('self', on_delete=models.PROTECT, related_name='second_children', db_index=True, null=True)
-    hours_updated = models.DateTimeField(null=True, blank=True)
+    hours_updated = models.DateTimeField(null=True, blank=True, db_index=True)
     default_status = models.IntegerField(choices=Status.choices, default=Status.UNDEFINED)
 
-    class Meta:
+    class Meta(BaseModel.Meta):
         verbose_name = _('Target')
         verbose_name_plural = _('Targets')
 
@@ -74,26 +83,29 @@ class Target(BaseModel):
 class Keyword(BaseModel):
     targets = models.ManyToManyField(Target, related_name='keywords', db_index=True)
 
-    class Meta:
+    class Meta(BaseModel.Meta):
         verbose_name = _('Keyword')
         verbose_name_plural = _('Keywords')
 
 
 class Period(BaseModel):
     target = models.ForeignKey(Target, on_delete=models.PROTECT, related_name='periods', db_index=True)
-    status = models.IntegerField(choices=Status.choices, default=Status.OPEN)
-    period = models.DateTimeRangeField()
+    status = models.IntegerField(choices=Status.choices, default=Status.OPEN, db_index=True)
+    period = DateTimeRangeField()
 
-    class Meta:
+    class Meta(BaseModel.Meta):
         verbose_name = _('Period')
         verbose_name_plural = _('Periods')
+        indexes = [
+            GistIndex(fields=['period'])
+        ]
 
 
 class Opening(models.Model):
     period = models.ForeignKey(Period, on_delete=models.CASCADE, related_name='openings', db_index=True)
     weekday = models.IntegerField(choices=Weekday.choices)
-    status = models.IntegerField(choices=Status.choices, default=Status.OPEN)
-    opening = models.DateTimeRangeField()
+    status = models.IntegerField(choices=Status.choices, default=Status.OPEN, db_index=True)
+    opening = DateTimeRangeField()
     description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
     # by default, all openings are for the first week of the rule, i.e. rotation of 1 week
     week = models.IntegerField(verbose_name=_('Week number'), default=1)
@@ -106,3 +118,6 @@ class Opening(models.Model):
     class Meta:
         verbose_name = _('Opening')
         verbose_name_plural = _('Openings')
+        indexes = [
+            GistIndex(fields=['opening'])
+        ]

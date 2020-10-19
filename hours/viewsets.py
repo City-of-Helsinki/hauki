8f478fa79360ccb28fa15f616e858e0ec2e49612
Mikko Keskinen <mikko.keskinen@hel.fi>
2020-10-19 12:27:19 +0300

    Add ability to retrieve Resource by its DataSource link (ResourceOrigin)

--- hours/viewsets.py
+++ hours/viewsets.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
 from .models import DatePeriod, OpeningHours, Resource, Rule
 from .serializers import (
@@ -14,6 +16,32 @@ class ResourceViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Resource.objects.all().order_by("id")
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        pk = self.kwargs.get(lookup_url_kwarg, None)
+        if not pk:
+            raise Http404
+
+        if ":" in pk:
+            # Find the object using resource origin
+            data_source_id, origin_id = pk.split(":")
+            filter_kwargs = {
+                "resourceorigin__data_source_id": data_source_id,
+                "resourceorigin__origin_id": origin_id,
+            }
+        else:
+            filter_kwargs = {self.lookup_field: pk}
+
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
 
 class DatePeriodViewSet(viewsets.ModelViewSet):

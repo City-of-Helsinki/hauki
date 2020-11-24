e3ec21d24f7458f91cf1f31813c6c5c19ab90d93
Mikko Keskinen <mikko.keskinen@hel.fi>
2020-11-24 09:50:56 +0200

    Change to mostly default Django User admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
-admin.site.register(User)
+
+class HaukiUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class HaukiUserAdmin(UserAdmin):
+    model = User
+    form = HaukiUserChangeForm
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Helusers"), {"fields": ("uuid", "department_name", "ad_groups")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(User, HaukiUserAdmin)

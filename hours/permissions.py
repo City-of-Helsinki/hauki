54d85b2ae63b96f12fea271d77a57dd9332b141c
Mikko Keskinen <mikko.keskinen@hel.fi>
2020-12-03 11:59:29 +0200

    Change filter_queryset_by_permission to handle only the read permission filtering. To make the permission checks in the viewsets easier to reason about.
    Additionally filter every viewsets queryset with the read permissions.

--- hours/permissions.py
+++ hours/permissions.py
@@ -1,69 +1,70 @@
+from functools import reduce
+
 from django.db.models import Q
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from hours.models import DatePeriod, Resource, Rule, TimeSpan
 
 
-def filter_queryset_by_permission(request, queryset):
-    """
-    Returns the queryset filtered by permission of request. Returned queryset
-    only contains objects of organizations the request has rights to.
+def deep_getattr(obj, attrs):
+    return reduce(lambda o, key: getattr(o, key), attrs.split("__"), obj)
+
+
+def get_path_to_resource(klass):
+    if klass == Resource:
+        return ""
+    elif klass == DatePeriod:
+        return "resource__"
+    elif klass == Rule:
+        return "group__period__resource__"
+    elif klass == TimeSpan:
+        return "group__period__resource__"
+
+    return None
+
+
+def filter_queryset_by_permission(user, queryset):
+    """Returns the queryset filtered by permission of the request
+
+    Returned queryset contains only objects of organizations the request has rights to.
 
     Resource parents and their parents organizations, if present, override
     access to a child resource. Similarly, parent publicity is a requirement
     for resource publicity.
     """
-    if request.user.is_superuser:
+    if user.is_superuser:
         return queryset
-    if request.user and request.user.is_authenticated:
-        users_organizations = request.user.get_all_organizations()
+
+    if user and user.is_authenticated:
+        users_organizations = user.get_all_organizations()
     else:
         users_organizations = set()
 
-    if queryset.model == Resource:
-        children_to_check = queryset
-        path_to_resource = ""
-    elif queryset.model == DatePeriod:
-        children_to_check = Resource.objects.filter(date_periods__in=queryset)
-        path_to_resource = "resource__"
-    elif queryset.model == Rule:
-        children_to_check = Resource.objects.filter(
-            date_periods__time_span_groups__rules__in=queryset
-        )
-        path_to_resource = "group__period__resource__"
-    elif queryset.model == TimeSpan:
-        children_to_check = Resource.objects.filter(
-            date_periods__time_span_groups__time_spans__in=queryset
-        )
-        path_to_resource = "group__period__resource__"
-    else:
+    path_to_resource = get_path_to_resource(queryset.model)
+
+    if path_to_resource is None:
         raise NotImplementedError(
-            "Permissions not defined for model {0}".format(queryset.model)
+            "Permissions not defined for class {0}".format(queryset.model)
         )
 
-    # if a resource has no parents, publicity and organization check is easy
-    is_public = Q(**{path_to_resource + "is_public": True})
-    has_request_organization = Q(**{path_to_resource + "parents__isnull": True}) & Q(
-        **{path_to_resource + "organization__in": users_organizations}
+    is_public = Q(**{path_to_resource + "is_public": True}) & (
+        Q(**{path_to_resource + "ancestry_is_public__isnull": True})
+        | Q(**{path_to_resource + "ancestry_is_public": True})
     )
 
-    # if a resource has parents, parents determine organizations that have rights
-    # and publicity
-    parents = Resource.objects.filter(children__in=children_to_check)
-    # multiple levels of parents will make the check slightly more complex
-    while parents:
-        has_request_organization |= Q(
-            **{path_to_resource + "parents__organization__in": users_organizations}
+    has_request_organization = (
+        Q(**{path_to_resource + "organization__in": users_organizations})
+        & Q(**{path_to_resource + "ancestry_organization__isnull": True})
+    ) | (
+        Q(
+            **{
+                path_to_resource
+                + "ancestry_organization__overlap": [o.id for o in users_organizations]
+            }
         )
-        is_public &= Q(**{path_to_resource + "parents__isnull": True}) | Q(
-            **{path_to_resource + "parents__is_public": True}
-        )
-        parents = Resource.objects.filter(children__in=parents)
-        path_to_resource += "parents__"
+    )
 
-    if request.method in SAFE_METHODS:
-        return queryset.filter(is_public | has_request_organization).distinct()
-    return queryset.filter(has_request_organization).distinct()
+    return queryset.filter(is_public | has_request_organization)
 
 
 class ReadOnlyPublic(BasePermission):
@@ -71,9 +72,48 @@ class ReadOnlyPublic(BasePermission):
         return request.method in SAFE_METHODS
 
     def has_object_permission(self, request, view, obj):
-        return bool(
-            filter_queryset_by_permission(request, type(obj).objects.filter(id=obj.id))
+        if request.method not in SAFE_METHODS:
+            return False
+
+        if request.user.is_superuser:
+            return True
+
+        if request.user and request.user.is_authenticated:
+            users_organizations = request.user.get_all_organizations()
+        else:
+            users_organizations = set()
+
+        path_to_resource = get_path_to_resource(obj.__class__)
+
+        if path_to_resource is None:
+            raise NotImplementedError(
+                "Permissions not defined for class {0}".format(obj.__class__)
+            )
+
+        resource_is_public = deep_getattr(obj, path_to_resource + "is_public")
+        resource_ancestry_is_public = deep_getattr(
+            obj, path_to_resource + "ancestry_is_public"
         )
+        resource_organization = deep_getattr(obj, path_to_resource + "organization")
+        resource_ancestry_organization = deep_getattr(
+            obj, path_to_resource + "ancestry_organization"
+        )
+
+        if resource_is_public and (
+            resource_ancestry_is_public is None or resource_ancestry_is_public
+        ):
+            return True
+
+        if (
+            resource_organization in users_organizations
+            and not resource_ancestry_organization
+        ) or (
+            resource_ancestry_organization
+            and users_organizations.intersection(resource_ancestry_organization)
+        ):
+            return True
+
+        return False
 
 
 class IsMemberOrAdminOfOrganization(BasePermission):
@@ -86,6 +126,33 @@ class IsMemberOrAdminOfOrganization(BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return bool(
-            filter_queryset_by_permission(request, type(obj).objects.filter(id=obj.id))
+        if request.user.is_superuser:
+            return True
+
+        if request.user and request.user.is_authenticated:
+            users_organizations = request.user.get_all_organizations()
+        else:
+            users_organizations = set()
+
+        path_to_resource = get_path_to_resource(obj.__class__)
+
+        if path_to_resource is None:
+            raise NotImplementedError(
+                "Permissions not defined for class {0}".format(obj.__class__)
+            )
+
+        resource_organization = deep_getattr(obj, path_to_resource + "organization")
+        resource_ancestry_organization = deep_getattr(
+            obj, path_to_resource + "ancestry_organization"
         )
+
+        if (
+            resource_organization in users_organizations
+            and not resource_ancestry_organization
+        ) or (
+            resource_ancestry_organization
+            and users_organizations.intersection(resource_ancestry_organization)
+        ):
+            return True
+
+        return False

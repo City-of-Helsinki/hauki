ff65405533a429fb42dcca2edbdc7fb51d316af8
Riku Oja <riku.oja@kapsi.fi>
2021-02-05 13:44:39 +0200

    Update TPREK hours hourly while Hauki is still in development mode

--- deploy/run_maintenance.sh
+++ deploy/run_maintenance.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # This file is for any management commands Hauki needs to run hourly i.e. cronjobs
+
+# TPREK organization structure must be up to date so users have the correct organization rights
 ./manage.py import_organizations -c tprek http://www.hel.fi/palvelukarttaws/rest/v4/department/
-./manage.py hours_import tprek --resources
+# TPREK units and connections must be up to date so changed TPREK data is present in Hauki
+./manage.py hours_import tprek --resources --merge
+# Library opening hours must be up to date, because they will not be edited in Hauki
 ./manage.py hours_import kirjastot --openings
+
+# NOTE! Until hauki is in production, import opening hours from TPREK to stay up to date
+./manage.py hours_import tprek --openings --merge

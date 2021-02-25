55446f15fd2afe1faea6f93655a30d33ca8a68ef
Riku Oja <riku.oja@kapsi.fi>
2021-02-25 18:03:34 +0200

    Never raise rule exceptions; validate rules at save instead

--- hours/tests/test_opening_hours_api.py
+++ hours/tests/test_opening_hours_api.py
@@ -58,6 +58,92 @@ def test_opening_hours_date_period_no_opening_hours(
 
 
 @pytest.mark.django_db
+def test_opening_hours_date_period_with_hours(
+    admin_client,
+    resource,
+    date_period_factory,
+    time_span_group_factory,
+    time_span_factory,
+):
+    period = date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=datetime.date(year=2020, month=1, day=1),
+        end_date=datetime.date(year=2020, month=12, day=31),
+    )
+    time_span_group = time_span_group_factory(period=period)
+    time_span_factory(group=time_span_group)
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 30
+
+
+@pytest.mark.django_db
+def test_opening_hours_date_period_with_hours_and_rule(
+    admin_client,
+    resource,
+    date_period_factory,
+    time_span_group_factory,
+    time_span_factory,
+    rule_factory,
+):
+    period = date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=datetime.date(year=2020, month=1, day=1),
+        end_date=datetime.date(year=2020, month=12, day=31),
+    )
+    time_span_group = time_span_group_factory(period=period)
+    time_span_factory(group=time_span_group)
+    rule_factory(
+        group=time_span_group,
+        context="period",
+        subject="week",
+        frequency_ordinal=None,
+        frequency_modifier="even",
+    )
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 15
+
+
+@pytest.mark.django_db
 def test_opening_hours_two_resources_one_date_period(
     admin_client, resource_factory, date_period_factory
 ):
@@ -134,6 +220,194 @@ def test_opening_hours_two_resources_two_date_periods(
     resource_names = {i["resource"]["name"]["fi"] for i in response.data["results"]}
 
     assert resource_names == {resource.name_fi, resource2.name_fi}
+
+
+@pytest.mark.django_db
+def test_opening_hours_date_period_start_date_empty(
+    admin_client, resource, date_period_factory
+):
+    date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=None,
+        end_date=datetime.date(year=2020, month=12, day=31),
+    )
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 0
+
+
+@pytest.mark.django_db
+def test_opening_hours_date_period_end_date_empty(
+    admin_client, resource, date_period_factory
+):
+    date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=datetime.date(year=2020, month=1, day=1),
+        end_date=None,
+    )
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 0
+
+
+@pytest.mark.django_db
+def test_opening_hours_date_period_start_and_end_date_empty(
+    admin_client, resource, date_period_factory
+):
+    date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=None,
+        end_date=None,
+    )
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 0
+
+
+@pytest.mark.django_db
+def test_opening_hours_date_period_with_hours_start_and_end_date_empty(
+    admin_client,
+    resource,
+    date_period_factory,
+    time_span_group_factory,
+    time_span_factory,
+):
+    period = date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=None,
+        end_date=None,
+    )
+    time_span_group = time_span_group_factory(period=period)
+    time_span_factory(group=time_span_group)
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 30
+
+
+@pytest.mark.django_db
+def test_opening_hours_date_period_with_hours_and_rule_start_and_end_date_empty(
+    admin_client,
+    resource,
+    date_period_factory,
+    time_span_group_factory,
+    time_span_factory,
+    rule_factory,
+):
+    period = date_period_factory(
+        resource=resource,
+        name="Testperiod",
+        start_date=None,
+        end_date=None,
+    )
+    time_span_group = time_span_group_factory(period=period)
+    time_span_factory(group=time_span_group)
+    # period has no start date, so context cannot be period
+    rule_factory(
+        group=time_span_group,
+        context="month",
+        subject="week",
+        frequency_modifier="even",
+    )
+
+    url = reverse("opening_hours-list")
+
+    data = {
+        "start_date": "2020-11-01",
+        "end_date": "2020-11-30",
+    }
+
+    response = admin_client.get(
+        url,
+        data=data,
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200, "{} {}".format(
+        response.status_code, response.data
+    )
+
+    assert len(response.data["results"]) == 1
+    assert response.data["results"][0]["resource"]["name"]["fi"] == resource.name_fi
+    assert len(response.data["results"][0]["opening_hours"]) == 15
+
+
+#  Filters
 
 
 @pytest.mark.django_db

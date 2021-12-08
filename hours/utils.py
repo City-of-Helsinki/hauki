ab85a6128bde3efa7095f56453ce43266bc5be16
Mikko Keskinen <mikko.keskinen@hel.fi>
2021-12-08 10:48:11 +0200

    Add ability to filter resource list by id

--- hours/utils.py
+++ hours/utils.py
@@ -3,7 +3,7 @@ def get_resource_pk_filter(pk):
         return {"pk": pk}
 
     # Find the object using resource origin
-    data_source_id, origin_id = pk.split(":")
+    data_source_id, origin_id = pk.split(":", 1)
     return {
         "origins__data_source_id": data_source_id,
         "origins__origin_id": origin_id,
